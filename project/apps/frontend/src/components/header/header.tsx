6970450cca5ab8fcc8fbe33f9b42c445a00a118c
Дмитрий <shcheklein.dev@gmail.com>
2024-06-18 23:46:49 +0300

    fix: layouts

--- project/apps/frontend/src/components/header/header.tsx
+++ project/apps/frontend/src/components/header/header.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import classNames from 'classnames';
 import MainNav from '../main-nav/main-nav';
 import { navItems } from '../main-nav/main-nav.data';
+import { AppRoute } from '../../shared/const';
+import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
   return (
     <header className={classNames('header')} id="header">
       <div className="container">
-        <span className="header__logo">
+        <Link to={AppRoute.Index} className="header__logo">
           <svg width="187" height="70" aria-hidden="true">
             <use xlinkHref="#logo"></use>
           </svg>
-        </span>
+        </Link>
 
         <MainNav items={navItems} />
 
