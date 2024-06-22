1f1d98806e11dc3fd785f62a13d0fd8504d8c935
Дмитрий <shcheklein.dev@gmail.com>
2024-06-22 20:16:20 +0300

    fix: remove temp navigation

--- project/apps/frontend/src/components/main-nav/main-nav.data.ts
+++ project/apps/frontend/src/components/main-nav/main-nav.data.ts
@@ -13,36 +13,4 @@ export const navItems = [
     icon: '#icon-user',
     ariaLabel: 'Личный кабинет',
   },
-  {
-    label: 'Friends',
-    href: AppRoute.Friends,
-    icon: '#icon-friends',
-    ariaLabel: 'Друзья',
-  },
-  {
-    label: 'Notifications',
-    href: '',
-    icon: '#icon-notification',
-    ariaLabel: 'Уведомления',
-    // notifications: [
-    //   {
-    //     text: 'Катерина пригласила вас на&nbsp;тренировку',
-    //     dateTime: '2023-12-23 12:35',
-    //     formattedTime: '23 декабря, 12:35',
-    //     isActive: true,
-    //   },
-    //   {
-    //     text: 'Никита отклонил приглашение на&nbsp;совместную тренировку',
-    //     dateTime: '2023-12-22 09:22',
-    //     formattedTime: '22 декабря, 09:22',
-    //     isActive: true,
-    //   },
-    //   {
-    //     text: 'Татьяна добавила вас в&nbsp;друзья',
-    //     dateTime: '2023-12-18 18:50',
-    //     formattedTime: '18 декабря, 18:50',
-    //     isActive: true,
-    //   },
-    // ],
-  },
 ];
