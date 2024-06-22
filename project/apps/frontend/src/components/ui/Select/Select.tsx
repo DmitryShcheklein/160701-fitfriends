ae37dca6d84678a49a4e290c830ea4848a68416b
Дмитрий <shcheklein.dev@gmail.com>
2024-06-22 19:09:52 +0300

    fix: select

--- project/apps/frontend/src/components/ui/Select/Select.tsx
+++ project/apps/frontend/src/components/ui/Select/Select.tsx
@@ -1,38 +1,9 @@
 import React from 'react';
-import Select, {
-  components,
-  OptionProps,
-  SingleValueProps,
-} from 'react-select';
+import Select, { components, Props } from 'react-select';
 
-interface CustomSelectProps {
-  options: { value: string; label: string }[];
-  placeholder?: string;
-  value: { value: string; label: string } | null;
-  onChange: (selectedOption: { value: string; label: string } | null) => void;
-}
+const CustomSelect = (props: Props) => {
+  const { value } = props;
 
-const CustomSelect: React.FC<CustomSelectProps> = ({
-  options,
-  placeholder,
-  value,
-  onChange,
-}) => {
-  // Кастомизация компонента SingleValue
-  const SingleValue = (props: SingleValueProps<any>) => (
-    <components.SingleValue {...props}>
-      <span className="custom-select__text1">{props.data.label}</span>
-    </components.SingleValue>
-  );
-
-  // Кастомизация компонента Option
-  const Option = (props: OptionProps<any>) => (
-    <components.Option {...props}>
-      <span className="custom-select__list-item1">{props.data.label}</span>
-    </components.Option>
-  );
-
-  // Кастомизация компонента DropdownIndicator
   const DropdownIndicator = (props: any) => (
     <components.DropdownIndicator {...props}>
       <span className="custom-select__icon">
@@ -43,15 +14,29 @@ const CustomSelect: React.FC<CustomSelectProps> = ({
     </components.DropdownIndicator>
   );
 
-  // Кастомизация контейнера Select
   const customStyles = {
-    control: (base: any) => ({
+    control: (base: any, state: any) => ({
       ...base,
-      border: 0,
-      boxShadow: 'none',
-      backgroundColor: 'transparent',
-      minHeight: 'auto',
-      height: 'auto',
+      display: 'flex',
+      alignItems: 'center',
+      width: '100%',
+      height: '50px',
+      padding: '0px 20px',
+      backgroundColor: state.isDisabled
+        ? 'rgba(174, 174, 174, 0.08)'
+        : state.isFocused || state.hasValue
+        ? 'transparent'
+        : 'transparent',
+      border: state.isDisabled
+        ? '1px solid rgba(24, 24, 24, 0.08)'
+        : state.isFocused || state.hasValue
+        ? '1px solid rgba(24, 24, 24, 0.1)'
+        : '1px solid rgba(24, 24, 24, 0.1)',
+      borderRadius: '8px',
+      boxShadow: state.isFocused ? '0 0 0 2px #c5ec2a' : 'none',
+      cursor: 'pointer',
+      transition:
+        'border 0.3s ease, color 0.3s ease, background-color 0.3s ease, box-shadow 0.3s ease',
     }),
     valueContainer: (base: any) => ({
       ...base,
@@ -62,22 +47,54 @@ const CustomSelect: React.FC<CustomSelectProps> = ({
       margin: '0',
       padding: '0',
     }),
-    placeholder: (base: any) => ({
+    placeholder: (base: any, state: any) => ({
       ...base,
-      margin: '0',
-      padding: '0',
+      position: 'absolute',
+      bottom: '17px',
+      left: '20px',
+      color: state.isDisabled ? 'rgba(51, 51, 51, 0.5)' : '#333333',
+      pointerEvents: 'none',
     }),
     indicatorsContainer: (base: any) => ({
       ...base,
       padding: '0',
     }),
+    indicatorContainer: (base: any) => ({
+      ...base,
+      padding: '0',
+    }),
+    indicatorSeparator: (base: any) => ({
+      ...base,
+      display: 'none',
+    }),
     menu: (base: any) => ({
       ...base,
-      marginTop: '0',
+      marginTop: 6,
+      border: '1px solid #CFCFCF',
+      borderRadius: 8,
+      padding: '2px',
+      maxHeight: '275px',
+      overflowY: 'auto',
+      backgroundColor: '#ffffff',
+      boxShadow: 'none',
     }),
     menuList: (base: any) => ({
       ...base,
-      padding: '0',
+      padding: 0,
+    }),
+    option: (base: any, state: any) => ({
+      ...base,
+      padding: 16,
+      cursor: 'pointer',
+      backgroundColor: state.isSelected
+        ? '#d5ff2d'
+        : state.isFocused
+        ? 'rgba(0, 0, 0, 0.4)'
+        : 'transparent',
+      color: '#000',
+      transition: 'background-color 0.3s ease',
+      borderRadius: 8,
+      marginBottom: 1,
     }),
   };
 
@@ -89,14 +106,10 @@ const CustomSelect: React.FC<CustomSelectProps> = ({
     >
       <span className="custom-select__label">Ваша локация</span>
       <Select
-        components={{ SingleValue, Option, DropdownIndicator }}
-        // styles={customStyles}
-        options={options}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
+        {...props}
+        components={{ DropdownIndicator }}
+        styles={customStyles}
         classNamePrefix="custom-select"
-        isClearable
       />
     </div>
   );
