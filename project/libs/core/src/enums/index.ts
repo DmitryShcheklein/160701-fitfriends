0b75f619022ca21c984ccc1d6856f7dfaa2b7848
Дмитрий <shcheklein.dev@gmail.com>
2024-06-26 11:59:43 +0300

    fix: enums

--- project/libs/core/src/enums/index.ts
+++ project/libs/core/src/enums/index.ts
@@ -6,6 +6,9 @@ export { RabbitQueue } from './rabbit/rabbit-queue.enum';
 export { UserRole } from './user/user-role.enum';
 export { UserGender } from './user/user-gender.enum';
 export { UserLocation } from './user/user-location.enum';
+export { FitnessLevel } from './user/fitness-level.enum';
+export { WorkoutDuration } from './user/workout-duration.enum';
+export { WorkoutType } from './user/workout-type.enum';
 // Sort
 export { SortBy } from './sort-by';
 export { SortDirection } from './sort-direction';
