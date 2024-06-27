f6da05f429c5b458ba44d08330d430b8543c3093
Dmitry <shcheklein.dev@gmail.com>
2024-06-27 22:45:21 +0300

    feat: add enums

---
+++ project/apps/frontend/src/enums/index.ts
@@ -0,0 +1,10 @@
+// User
+export { UserRole } from './user/user-role.enum';
+export { UserGender } from './user/user-gender.enum';
+export { UserLocation } from './user/user-location.enum';
+export { FitnessLevel } from './user/fitness-level.enum';
+export { WorkoutDuration } from './user/workout-duration.enum';
+export { WorkoutType } from './user/workout-type.enum';
+// Sort
+export { SortBy } from './sort-by';
+export { SortDirection } from './sort-direction';
