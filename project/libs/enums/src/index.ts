85e70854c92b091005959039cd70e683c2f512d3
Дмитрий <shcheklein.dev@gmail.com>
2024-06-28 08:51:48 +0300

    fix: enums

---
+++ project/libs/enums/src/index.ts
@@ -0,0 +1,10 @@
+// User
+export { UserRole } from './lib/user/user-role.enum';
+export { UserGender } from './lib/user/user-gender.enum';
+export { UserLocation } from './lib/user/user-location.enum';
+export { FitnessLevel } from './lib/user/fitness-level.enum';
+export { WorkoutDuration } from './lib/user/workout-duration.enum';
+export { WorkoutType } from './lib/user/workout-type.enum';
+// Sort
+export { SortBy } from './lib/sort-by';
+export { SortDirection } from './lib/sort-direction';
