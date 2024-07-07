4707f5e7366341eac9fb132d3d8d32e80a30be77
Дмитрий <shcheklein.dev@gmail.com>
2024-07-07 13:19:36 +0300

    fix: remove old version seed

--- project/apps/backend/src/seed/seed.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SeedService } from './seed.service';
-import { AuthenticationModule } from '@project/auth-module';
-
-@Module({
-  imports: [AuthenticationModule],
-  providers: [SeedService],
-})
-export class SeedModule {}
