e61d2989d77f631ddf69ba7efd1effbea78d93a6
Дмитрий <shcheklein.dev@gmail.com>
2024-10-21 14:44:28 +0300

    feat: training-card.tsx, comments.tsx layout

---
+++ project/apps/frontend/src/components/training-card/training-card.tsx
@@ -0,0 +1,186 @@
+import Popup from '../ui/popup/popup';
+import { BuyForm } from '../forms/buy-form/buy-form';
+import { useGetTrainingByIdQuery } from '../../store/training-process/training-api';
+import {
+  genderOptions,
+  specializationOptions,
+  workoutDurationOptions,
+} from '../forms/user-info/user-info.data';
+import { useState } from 'react';
+
+interface TrainingCardProps {
+  trainingId: string;
+}
+
+export const TrainingCard = ({ trainingId }: TrainingCardProps) => {
+  const { data: training } = useGetTrainingByIdQuery(trainingId);
+
+  const trainingType = specializationOptions
+    .find((el) => el.value === training?.trainingType)
+    ?.label?.toLowerCase();
+  const genderType = genderOptions
+    .find((el) => el.value === training?.gender)
+    ?.label?.toLowerCase();
+  const workoutDuration = workoutDurationOptions
+    .find((el) => el.value === training?.duration)
+    ?.label.replace('-', '_')
+    .replace(' мин', 'минут');
+
+  const [showBuyModal, setShowBuyModal] = useState(false);
+  const onButtonBuyClick = () => {
+    setShowBuyModal(!showBuyModal);
+  };
+
+  return (
+    <div className="training-card">
+      <div className="training-info">
+        <h2 className="visually-hidden">Информация о тренировке</h2>
+        <div className="training-info__header">
+          <div className="training-info__coach">
+            <div className="training-info__coach-info">
+              <span className="training-info__label">Тренер</span>
+              <span className="training-info__name">{training?.coach}</span>
+            </div>
+          </div>
+        </div>
+        <div className="training-info__main-content">
+          <form action="#" method="get">
+            <div className="training-info__form-wrapper">
+              <div className="training-info__info-wrapper">
+                <div className="training-info__input training-info__input--training">
+                  <label>
+                    <span className="training-info__label">
+                      {training?.name}
+                    </span>
+                    <input
+                      type="text"
+                      name="training"
+                      defaultValue={training?.name}
+                      disabled
+                    />
+                  </label>
+                  <div className="training-info__error">Обязательное поле</div>
+                </div>
+                <div className="training-info__textarea">
+                  <label>
+                    <span className="training-info__label">
+                      Описание тренировки
+                    </span>
+                    <textarea
+                      name="description"
+                      disabled
+                      defaultValue={training?.description}
+                    />
+                  </label>
+                </div>
+              </div>
+              <div className="training-info__rating-wrapper">
+                <div className="training-info__input training-info__input--rating">
+                  <label>
+                    <span className="training-info__label">Рейтинг</span>
+                    <span className="training-info__rating-icon">
+                      <svg width="18" height="18" aria-hidden="true">
+                        <use xlinkHref="#icon-star"></use>
+                      </svg>
+                    </span>
+                    <input
+                      type="number"
+                      name="rating"
+                      defaultValue={training?.rating}
+                      disabled
+                    />
+                  </label>
+                </div>
+                <ul className="training-info__list">
+                  <li className="training-info__item">
+                    <div className="hashtag hashtag--white">
+                      <span>#{trainingType}</span>
+                    </div>
+                  </li>
+                  <li className="training-info__item">
+                    <div className="hashtag hashtag--white">
+                      <span>#{genderType}</span>
+                    </div>
+                  </li>
+                  <li className="training-info__item">
+                    <div className="hashtag hashtag--white">
+                      <span>#{training?.calories}ккал</span>
+                    </div>
+                  </li>
+                  <li className="training-info__item">
+                    <div className="hashtag hashtag--white">
+                      <span>#{workoutDuration}</span>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <div className="training-info__price-wrapper">
+                <div className="training-info__input training-info__input--price">
+                  <label>
+                    <span className="training-info__label">Стоимость</span>
+                    <input
+                      type="text"
+                      name="price"
+                      defaultValue={`${training?.price} ₽`}
+                      disabled
+                    />
+                  </label>
+                  <div className="training-info__error">Введите число</div>
+                </div>
+                <button
+                  onClick={onButtonBuyClick}
+                  className="btn training-info__buy"
+                  type="button"
+                >
+                  Купить
+                </button>
+                <Popup
+                  isOpen={showBuyModal}
+                  title="Купить тренировку"
+                  showCloseButton
+                  onClose={() => setShowBuyModal(false)}
+                >
+                  <BuyForm
+                    training={training}
+                    onSuccess={() => setShowBuyModal(false)}
+                  />
+                </Popup>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="training-video">
+        <h2 className="training-video__title">Видео</h2>
+        <div className="training-video__video">
+          <div className="training-video__thumbnail">
+            <video src={training?.video} controls loop></video>
+          </div>
+          <button
+            className="training-video__play-button btn-reset"
+            style={{ display: 'none' }}
+          >
+            <svg width="18" height="30" aria-hidden="true">
+              <use xlinkHref="#icon-arrow"></use>
+            </svg>
+          </button>
+        </div>
+        <div className="training-video__buttons-wrapper">
+          <button
+            className="btn training-video__button training-video__button--start"
+            type="button"
+            disabled
+          >
+            Приступить
+          </button>
+          <button
+            className="btn training-video__button training-video__button--stop"
+            type="button"
+          >
+            Закончить
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
--- project/apps/frontend/src/pages/training-card/training-card.tsx
+++ project/apps/frontend/src/pages/training-card/training-card.tsx
@@ -3,40 +3,16 @@ import { AppRoute, PageTitles } from '../../shared/const';
 import { Sidebar } from '../../components/base/sidebar/sidebar';
 import { Link, useParams } from 'react-router-dom';
 import { useGetTrainingByIdQuery } from '../../store/training-process/training-api';
-import { useGetCommentsByTrainingIdQuery } from '../../store/comments-process/comments-api';
 import { LoaderPage } from '../../components/loaders/loader-page/loader-page';
-import {
-  specializationOptions,
-  genderOptions,
-  workoutDurationOptions,
-} from '../../components/forms/user-info/user-info.data';
-import Popup from '../../components/ui/popup/popup';
-import { useState } from 'react';
-import ReviewForm from '../../components/forms/review-form/review-form';
-import { BuyForm } from '../../components/forms/buy-form/buy-form';
+import { Comments } from '../../components/comments/comments';
+import { TrainingCard } from '../../components/training-card/training-card';
 
 const TrainingCardPage = () => {
   const { id } = useParams<{ id: string }>();
   const trainingId = String(id);
-  const { data: comments } = useGetCommentsByTrainingIdQuery(trainingId);
-  const { data: training, isLoading } = useGetTrainingByIdQuery(trainingId);
 
-  const trainingType = specializationOptions
-    .find((el) => el.value === training?.trainingType)
-    ?.label?.toLowerCase();
-  const genderType = genderOptions
-    .find((el) => el.value === training?.gender)
-    ?.label?.toLowerCase();
-  const workoutDuration = workoutDurationOptions
-    .find((el) => el.value === training?.duration)
-    ?.label.replace('-', '_')
-    .replace(' мин', 'минут');
+  const { isLoading } = useGetTrainingByIdQuery(trainingId);
 
-  const [showReviewModal, setShowReviewModal] = useState(false);
-  const [showBuyModal, setShowBuyModal] = useState(false);
-  const onButtonBuyClick = () => {
-    setShowBuyModal(!showBuyModal);
-  };
   if (isLoading) return <LoaderPage />;
 
   return (
@@ -57,217 +33,10 @@ const TrainingCardPage = () => {
             </svg>
             <span>Назад</span>
           </Link>
-          {comments?.length ? (
-            <>
-              <h2 className="reviews-side-bar__title">Отзывы</h2>
-              <ul className="reviews-side-bar__list">
-                {comments.map(({ id, message, rating, user }) => {
-                  return (
-                    <li className="reviews-side-bar__item" key={id}>
-                      <div className="review">
-                        <div className="review__user-info">
-                          {user.avatarPath ? (
-                            <div className="review__user-photo">
-                              <picture>
-                                <img
-                                  src={user.avatarPath}
-                                  width="64"
-                                  height="64"
-                                  alt="Изображение пользователя"
-                                />
-                              </picture>
-                            </div>
-                          ) : null}
-                          <span className="review__user-name">
-                            {user.firstName}
-                          </span>
-                          <div className="review__rating">
-                            <svg width="16" height="16" aria-hidden="true">
-                              <use xlinkHref="#icon-star"></use>
-                            </svg>
-                            <span>{rating}</span>
-                          </div>
-                        </div>
-                        <p className="review__comment">{message}</p>
-                      </div>
-                    </li>
-                  );
-                })}
-              </ul>
-            </>
-          ) : null}
-
-          <button
-            className="btn btn--medium reviews-side-bar__button"
-            type="button"
-            onClick={() => setShowReviewModal(true)}
-          >
-            Оставить отзыв
-          </button>
-          <Popup
-            isOpen={showReviewModal}
-            title="Оставить отзыв"
-            showCloseButton
-            onClose={() => setShowReviewModal(false)}
-          >
-            <ReviewForm
-              id={trainingId}
-              onSuccess={() => setShowReviewModal(false)}
-            />
-          </Popup>
+          <Comments trainingId={trainingId} />
         </Sidebar>
-        <div className="training-card">
-          <div className="training-info">
-            <h2 className="visually-hidden">Информация о тренировке</h2>
-            <div className="training-info__header">
-              <div className="training-info__coach">
-                <div className="training-info__coach-info">
-                  <span className="training-info__label">Тренер</span>
-                  <span className="training-info__name">{training?.coach}</span>
-                </div>
-              </div>
-            </div>
-            <div className="training-info__main-content">
-              <form action="#" method="get">
-                <div className="training-info__form-wrapper">
-                  <div className="training-info__info-wrapper">
-                    <div className="training-info__input training-info__input--training">
-                      <label>
-                        <span className="training-info__label">
-                          {training?.name}
-                        </span>
-                        <input
-                          type="text"
-                          name="training"
-                          defaultValue={training?.name}
-                          disabled
-                        />
-                      </label>
-                      <div className="training-info__error">
-                        Обязательное поле
-                      </div>
-                    </div>
-                    <div className="training-info__textarea">
-                      <label>
-                        <span className="training-info__label">
-                          Описание тренировки
-                        </span>
-                        <textarea
-                          name="description"
-                          disabled
-                          defaultValue={training?.description}
-                        />
-                      </label>
-                    </div>
-                  </div>
-                  <div className="training-info__rating-wrapper">
-                    <div className="training-info__input training-info__input--rating">
-                      <label>
-                        <span className="training-info__label">Рейтинг</span>
-                        <span className="training-info__rating-icon">
-                          <svg width="18" height="18" aria-hidden="true">
-                            <use xlinkHref="#icon-star"></use>
-                          </svg>
-                        </span>
-                        <input
-                          type="number"
-                          name="rating"
-                          defaultValue={training?.rating}
-                          disabled
-                        />
-                      </label>
-                    </div>
-                    <ul className="training-info__list">
-                      <li className="training-info__item">
-                        <div className="hashtag hashtag--white">
-                          <span>#{trainingType}</span>
-                        </div>
-                      </li>
-                      <li className="training-info__item">
-                        <div className="hashtag hashtag--white">
-                          <span>#{genderType}</span>
-                        </div>
-                      </li>
-                      <li className="training-info__item">
-                        <div className="hashtag hashtag--white">
-                          <span>#{training?.calories}ккал</span>
-                        </div>
-                      </li>
-                      <li className="training-info__item">
-                        <div className="hashtag hashtag--white">
-                          <span>#{workoutDuration}</span>
-                        </div>
-                      </li>
-                    </ul>
-                  </div>
-                  <div className="training-info__price-wrapper">
-                    <div className="training-info__input training-info__input--price">
-                      <label>
-                        <span className="training-info__label">Стоимость</span>
-                        <input
-                          type="text"
-                          name="price"
-                          defaultValue={`${training?.price} ₽`}
-                          disabled
-                        />
-                      </label>
-                      <div className="training-info__error">Введите число</div>
-                    </div>
-                    <button
-                      onClick={onButtonBuyClick}
-                      className="btn training-info__buy"
-                      type="button"
-                    >
-                      Купить
-                    </button>
-                    <Popup
-                      isOpen={showBuyModal}
-                      title="Купить тренировку"
-                      showCloseButton
-                      onClose={() => setShowBuyModal(false)}
-                    >
-                      <BuyForm
-                        training={training}
-                        onSuccess={() => setShowBuyModal(false)}
-                      />
-                    </Popup>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
-          <div className="training-video">
-            <h2 className="training-video__title">Видео</h2>
-            <div className="training-video__video">
-              <div className="training-video__thumbnail">
-                <video src={training?.video} controls loop></video>
-              </div>
-              <button
-                className="training-video__play-button btn-reset"
-                style={{ display: 'none' }}
-              >
-                <svg width="18" height="30" aria-hidden="true">
-                  <use xlinkHref="#icon-arrow"></use>
-                </svg>
-              </button>
-            </div>
-            <div className="training-video__buttons-wrapper">
-              <button
-                className="btn training-video__button training-video__button--start"
-                type="button"
-                disabled
-              >
-                Приступить
-              </button>
-              <button
-                className="btn training-video__button training-video__button--stop"
-                type="button"
-              >
-                Закончить
-              </button>
-            </div>
-          </div>
-        </div>
+
+        <TrainingCard trainingId={trainingId} />
       </div>
     </>
   );
