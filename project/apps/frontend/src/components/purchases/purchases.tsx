967fb0bcf96b1c69b8619f7a220b3d0d0d1e5f64
Дмитрий <shcheklein.dev@gmail.com>
2024-11-08 18:15:50 +0300

    fix: orders layout

--- project/apps/frontend/src/components/purchases/purchases.tsx
+++ project/apps/frontend/src/components/purchases/purchases.tsx
@@ -54,35 +54,33 @@ export const Purchases = () => {
         <div className="my-purchases__title-wrapper">
           <h1 className="my-purchases__title">Мои покупки</h1>
 
-          {items.length ? (
-            <div className="my-purchases__controls">
-              <div
-                className="custom-toggle custom-toggle--switch custom-toggle--switch-right my-purchases__switch"
-                data-validate-type="checkbox"
-              >
-                <label>
-                  <input
-                    onChange={() => {
-                      setCurrentPage(1);
-                      setItems([]);
-                      setFilter({
-                        isActive: !filter.isActive,
-                      });
-                    }}
-                    type="checkbox"
-                    value="user-agreement-1"
-                    name="user-agreement"
-                  />
-                  <span className="custom-toggle__icon">
-                    <svg width="9" height="6" aria-hidden="true">
-                      <use xlinkHref="#arrow-check"></use>
-                    </svg>
-                  </span>
-                  <span className="custom-toggle__label">Только активные</span>
-                </label>
-              </div>
+          <div className="my-purchases__controls">
+            <div
+              className="custom-toggle custom-toggle--switch custom-toggle--switch-right my-purchases__switch"
+              data-validate-type="checkbox"
+            >
+              <label>
+                <input
+                  onChange={() => {
+                    setCurrentPage(1);
+                    setItems([]);
+                    setFilter({
+                      isActive: !filter.isActive,
+                    });
+                  }}
+                  type="checkbox"
+                  value="user-agreement-1"
+                  name="user-agreement"
+                />
+                <span className="custom-toggle__icon">
+                  <svg width="9" height="6" aria-hidden="true">
+                    <use xlinkHref="#arrow-check"></use>
+                  </svg>
+                </span>
+                <span className="custom-toggle__label">Только активные</span>
+              </label>
             </div>
-          ) : null}
+          </div>
         </div>
         {!items.length ? <h3 className="">Список покупок пуст</h3> : null}
         {items.length ? (
