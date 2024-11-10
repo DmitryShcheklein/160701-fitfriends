bcb08b8189e62329ea51d9902b52556e67e70d03
Дмитрий <shcheklein.dev@gmail.com>
2024-11-10 11:43:04 +0300

    fix: 404 page

--- project/apps/frontend/src/components/base/error-screen/error-screen.tsx
+++ project/apps/frontend/src/components/base/error-screen/error-screen.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { AppRoute } from '../../../shared/const';
+import Popup from '../../ui/popup/popup';
 
 type TErrorScreen = {
   redirectTo?: string;
@@ -10,26 +11,21 @@ const ErrorScreen = ({
   redirectTo = AppRoute.Intro,
   redirectMessage = 'Перейти на главную',
 }: TErrorScreen) => (
-  <div className="popup-form popup-form--sign-in">
-    <div className="popup-form__wrapper">
-      <div className="popup-form__content">
-        <div className="popup-form__title-wrapper">
-          <h1 className="popup-form__title">Ошибка 404</h1>
-        </div>
-        <div className="popup-form__form">
-          <h2 className="sign-up__legend">Страница не найдена.</h2>
-          <p className="error__text">
-            Возможно, страница была удалена или её вовсе не существовало.
-          </p>
-          <div className="sign-up">
-            <Link className="btn sign-up__button" to={redirectTo}>
-              {redirectMessage}
-            </Link>
-          </div>
+  <Popup isStatic title="Ошибка 404" className="popup-form popup-form--sign-in">
+    <div className="popup-form__content">
+      <div className="popup-form__form">
+        <h2 className="sign-up__legend">Страница не найдена.</h2>
+        <p className="error__text">
+          Возможно, страница была удалена или её вовсе не существовало.
+        </p>
+        <div className="sign-up">
+          <Link className="btn sign-up__button" to={redirectTo}>
+            {redirectMessage}
+          </Link>
         </div>
       </div>
     </div>
-  </div>
+  </Popup>
 );
 
 export default ErrorScreen;
