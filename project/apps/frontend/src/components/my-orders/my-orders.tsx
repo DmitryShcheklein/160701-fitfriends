b44d09869cb49ed5fb53a8e7e7346ea317571acd
Дмитрий <shcheklein.dev@gmail.com>
2024-11-18 23:27:30 +0300

    fix: my orders layout

--- project/apps/frontend/src/components/my-orders/my-orders.tsx
+++ project/apps/frontend/src/components/my-orders/my-orders.tsx
@@ -27,18 +27,20 @@ export const MyOrders = () => {
           <div className="sort-for">
             <p>Сортировать по:</p>
             <div className="sort-for__btn-container">
-              <button className="btn-filter-sort" type="button">
-                <span>Сумме</span>
-                <svg width="16" height="10" aria-hidden="true">
-                  <use xlinkHref="#icon-sort-up"></use>
-                </svg>
-              </button>
-              <button className="btn-filter-sort" type="button">
-                <span>Количеству</span>
-                <svg width="16" height="10" aria-hidden="true">
-                  <use xlinkHref="#icon-sort-down"></use>
-                </svg>
-              </button>
+              {[
+                { name: 'Сумме' },
+                {
+                  name: 'Количеству',
+                },
+              ].map(({ name, icon }) => (
+                <button className="btn-filter-sort" type="button">
+                  <span>{name}</span>
+                  <svg width="16" height="10" aria-hidden="true">
+                    <use xlinkHref="#icon-sort-up"></use>
+                    {/*<use xlinkHref="#icon-sort-down"></use>*/}
+                  </svg>
+                </button>
+              ))}
             </div>
           </div>
         </div>
