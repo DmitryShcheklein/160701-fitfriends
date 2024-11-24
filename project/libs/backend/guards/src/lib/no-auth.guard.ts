339627bb83966ae49697453a01a00ff2571250d7
Дмитрий <shcheklein.dev@gmail.com>
2024-11-24 18:24:58 +0300

    feat: roles guard

--- project/libs/backend/guards/src/lib/no-auth.guard.ts
+++ project/libs/backend/guards/src/lib/no-auth.guard.ts
@@ -4,6 +4,7 @@ import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 export class NotAuthenticatedGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest();
+
     return !request.user; // Проверяем, что пользователь не аутентифицирован
   }
 }
