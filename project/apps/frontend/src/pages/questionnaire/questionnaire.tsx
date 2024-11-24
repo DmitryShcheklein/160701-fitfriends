ba8fadc0551958c490775fd4f5deaf8c20c6d6bd
Дмитрий <shcheklein.dev@gmail.com>
2024-11-24 16:35:12 +0300

    fix: questionnaire.tsx

--- project/apps/frontend/src/pages/questionnaire/questionnaire.tsx
+++ project/apps/frontend/src/pages/questionnaire/questionnaire.tsx
@@ -3,15 +3,18 @@ import { getPageTitle } from '../../shared/const';
 import { QuestionnaireUser } from '../../components/forms/questionnaire/user/questionnaire-user';
 import React from 'react';
 import { QuestionnaireTrainer } from '../../components/forms/questionnaire/trainer/questionnaire-trainer';
+import { useAuthRole } from '../../hooks/useAuth';
 
 const QuestionnairePage = () => {
+  const { isTrainerAuth, isUserAuth } = useAuthRole();
+
   return (
     <>
       <Helmet>
         <title>{getPageTitle('Questionnaire')}</title>
       </Helmet>
-      {!!0 ? <QuestionnaireUser /> : null}
-      {!!1 ? <QuestionnaireTrainer /> : null}
+      {isUserAuth ? <QuestionnaireUser /> : null}
+      {isTrainerAuth ? <QuestionnaireTrainer /> : null}
     </>
   );
 };
